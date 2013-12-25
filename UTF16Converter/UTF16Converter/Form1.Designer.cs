6536bffbfa9f424ce91fc9a5f36b96775de6293a
masashi.sasaki <masashi.sasaki@solize-group.com>
2013-12-25 14:13:27 +0900

    UTF16Converter

---
+++ UTF16Converter/UTF16Converter/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace UTF16Converter
+{
+    partial class UTF16ConverterForm
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.toUTF16Button = new System.Windows.Forms.Button();
+            this.toJpButton = new System.Windows.Forms.Button();
+            this.utf16UCheckBox = new System.Windows.Forms.CheckBox();
+            this.convertTextLabel = new System.Windows.Forms.Label();
+            this.utf16TextLabel = new System.Windows.Forms.Label();
+            this.closeButton = new System.Windows.Forms.Button();
+            this.utf16TextBox = new System.Windows.Forms.TextBox();
+            this.jpTextBox = new System.Windows.Forms.TextBox();
+            this.smallCharCheckBox = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // toUTF16Button
+            // 
+            this.toUTF16Button.Location = new System.Drawing.Point(159, 216);
+            this.toUTF16Button.Name = "toUTF16Button";
+            this.toUTF16Button.Size = new System.Drawing.Size(75, 23);
+            this.toUTF16Button.TabIndex = 1;
+            this.toUTF16Button.Text = "↓";
+            this.toUTF16Button.UseVisualStyleBackColor = true;
+            this.toUTF16Button.Click += new System.EventHandler(this.toUTF16Button_Click);
+            // 
+            // toJpButton
+            // 
+            this.toJpButton.Location = new System.Drawing.Point(265, 216);
+            this.toJpButton.Name = "toJpButton";
+            this.toJpButton.Size = new System.Drawing.Size(75, 23);
+            this.toJpButton.TabIndex = 1;
+            this.toJpButton.Text = "↑";
+            this.toJpButton.UseVisualStyleBackColor = true;
+            this.toJpButton.Click += new System.EventHandler(this.toJpButton_Click);
+            // 
+            // utf16UCheckBox
+            // 
+            this.utf16UCheckBox.AutoSize = true;
+            this.utf16UCheckBox.Checked = true;
+            this.utf16UCheckBox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.utf16UCheckBox.Location = new System.Drawing.Point(371, 220);
+            this.utf16UCheckBox.Name = "utf16UCheckBox";
+            this.utf16UCheckBox.Size = new System.Drawing.Size(147, 16);
+            this.utf16UCheckBox.TabIndex = 2;
+            this.utf16UCheckBox.Text = "utf16文字列に/uを付ける";
+            this.utf16UCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // convertTextLabel
+            // 
+            this.convertTextLabel.AutoSize = true;
+            this.convertTextLabel.Location = new System.Drawing.Point(12, 13);
+            this.convertTextLabel.Name = "convertTextLabel";
+            this.convertTextLabel.Size = new System.Drawing.Size(65, 12);
+            this.convertTextLabel.TabIndex = 3;
+            this.convertTextLabel.Text = "変換文字列";
+            // 
+            // utf16TextLabel
+            // 
+            this.utf16TextLabel.AutoSize = true;
+            this.utf16TextLabel.Location = new System.Drawing.Point(12, 227);
+            this.utf16TextLabel.Name = "utf16TextLabel";
+            this.utf16TextLabel.Size = new System.Drawing.Size(75, 12);
+            this.utf16TextLabel.TabIndex = 3;
+            this.utf16TextLabel.Text = "UTF16文字列";
+            // 
+            // closeButton
+            // 
+            this.closeButton.Location = new System.Drawing.Point(507, 4);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(75, 23);
+            this.closeButton.TabIndex = 4;
+            this.closeButton.Text = "閉じる";
+            this.closeButton.UseVisualStyleBackColor = true;
+            this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
+            // 
+            // utf16TextBox
+            // 
+            this.utf16TextBox.Location = new System.Drawing.Point(14, 245);
+            this.utf16TextBox.Multiline = true;
+            this.utf16TextBox.Name = "utf16TextBox";
+            this.utf16TextBox.Size = new System.Drawing.Size(574, 210);
+            this.utf16TextBox.TabIndex = 5;
+            // 
+            // jpTextBox
+            // 
+            this.jpTextBox.Location = new System.Drawing.Point(14, 33);
+            this.jpTextBox.Multiline = true;
+            this.jpTextBox.Name = "jpTextBox";
+            this.jpTextBox.Size = new System.Drawing.Size(574, 177);
+            this.jpTextBox.TabIndex = 6;
+            // 
+            // smallCharCheckBox
+            // 
+            this.smallCharCheckBox.AutoSize = true;
+            this.smallCharCheckBox.Checked = true;
+            this.smallCharCheckBox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.smallCharCheckBox.Location = new System.Drawing.Point(525, 220);
+            this.smallCharCheckBox.Name = "smallCharCheckBox";
+            this.smallCharCheckBox.Size = new System.Drawing.Size(60, 16);
+            this.smallCharCheckBox.TabIndex = 7;
+            this.smallCharCheckBox.Text = "小文字";
+            this.smallCharCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // UTF16ConverterForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(596, 463);
+            this.ControlBox = false;
+            this.Controls.Add(this.smallCharCheckBox);
+            this.Controls.Add(this.jpTextBox);
+            this.Controls.Add(this.utf16TextBox);
+            this.Controls.Add(this.closeButton);
+            this.Controls.Add(this.utf16TextLabel);
+            this.Controls.Add(this.convertTextLabel);
+            this.Controls.Add(this.utf16UCheckBox);
+            this.Controls.Add(this.toJpButton);
+            this.Controls.Add(this.toUTF16Button);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "UTF16ConverterForm";
+            this.ShowIcon = false;
+            this.Text = "UTF16Converter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button toUTF16Button;
+        private System.Windows.Forms.Button toJpButton;
+        private System.Windows.Forms.CheckBox utf16UCheckBox;
+        private System.Windows.Forms.Label convertTextLabel;
+        private System.Windows.Forms.Label utf16TextLabel;
+        private System.Windows.Forms.Button closeButton;
+        private System.Windows.Forms.TextBox utf16TextBox;
+        private System.Windows.Forms.TextBox jpTextBox;
+        private System.Windows.Forms.CheckBox smallCharCheckBox;
+    }
+}
+
---
+++ UTF16Converter/UTF16Converter/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace UTF16Converter
+{
+    public partial class UTF16ConverterForm : Form
+    {
+        private Dictionary<string, string> toUTFMap = new Dictionary<string, string>(4000);
+        private Dictionary<string, string> toJPMap = new Dictionary<string, string>(4000);
+        
+        public UTF16ConverterForm()
+        {
+            InitializeComponent();
+
+            InitializeDictionary();
+        }
+
+        private void InitializeDictionary()
+        {
+            ConvertDictionaryCreator.CreateToUtf16Map(toUTFMap);
+            ConvertDictionaryCreator.CreateReverseMap(toUTFMap, toJPMap);
+        }
+
+        private void toUTF16Button_Click(object sender, EventArgs e)
+        {
+            string text = jpTextBox.Text;
+
+            string result = string.Empty;
+
+            for (int i = 0; i < text.Length; ++i)
+            {
+                string key = text[i].ToString();
+                if(toUTFMap.ContainsKey(key))
+                {
+                    if (utf16UCheckBox.Checked)
+                    {
+                        result += "\\u";
+                    }
+
+                    string val = toUTFMap[key];
+                    if (smallCharCheckBox.Checked)
+                    {
+                        val = val.ToLower();
+                    }
+                    else
+                    {
+                        val = val.ToUpper();
+                    }
+
+                    result += val;
+                }
+            }
+
+            utf16TextBox.Text = result;
+        }
+
+        private string[] spritWord(string text, int wSize)
+        {
+            List<string> words = new List<string>(20);
+            
+            for (int p = 0; p < text.Length; p += 4)
+            {
+                if (wSize == 6)
+                {
+                    p += 2;
+                }
+
+                string word = text.Substring(p, 4);
+                words.Add(word);
+            }
+
+            return words.ToArray();
+        }
+
+        private void toJpButton_Click(object sender, EventArgs e)
+        {
+            string text = utf16TextBox.Text;
+            if (text.Length == 0)
+            {
+                return;
+            }
+
+            bool existU = (text.IndexOf("\\u") == 0 || text.IndexOf("\\U") == 0);
+            int splitLength = existU ? 6 : 4;
+
+            string[] words = spritWord(text, splitLength);
+
+            string result = string.Empty;
+            for (int i = 0; i < words.Length; ++i)
+            {
+                string key = words[i].ToUpper();
+                if (toJPMap.ContainsKey(key))
+                {
+                    result += toJPMap[key];
+                }
+            }
+
+            jpTextBox.Text = result;
+        }
+
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
